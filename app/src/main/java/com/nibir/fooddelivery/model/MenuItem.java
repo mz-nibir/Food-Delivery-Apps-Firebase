bb7a8f3053f09b646b9ea0409c16d4c95bbe2656
Nibir <mz.nibir@gmail.com>
2020-11-01 23:21:58 +0600

    create manuItem class

--- app/src/main/java/com/nibir/fooddelivery/model/MenuItem.java
+++ app/src/main/java/com/nibir/fooddelivery/model/MenuItem.java
@@ -1,4 +1,34 @@
 package com.nibir.fooddelivery.model;
 
 public class MenuItem {
+    String itemName;
+    String itemDescription;
+    String itemPrice;
+
+    public MenuItem() {
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 }
