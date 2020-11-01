1286db92b9b7cb37f6feb0dacb149f7edd0aa9a4
Nibir <mz.nibir@gmail.com>
2020-11-02 00:58:11 +0600

    data Uploaded succesfully

--- app/src/main/java/com/nibir/fooddelivery/MainActivity.java
+++ app/src/main/java/com/nibir/fooddelivery/MainActivity.java
@@ -1,12 +1,17 @@
 package com.nibir.fooddelivery;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.nibir.fooddelivery.model.Restaurant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -42,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
         myRestaurent.setRestaurentName("Nibir Restaurents");
         myRestaurent.setRestaurentDescription("Best restaurent in Dhaka");
         myRestaurent.setRestaurentLocation("Bashundhara,Dhaka");
+        myRestaurent.setRestaurentImgUrl("https://img.jakpost.net/c/2016/09/29/2016_09_29_12990_1475116504._large.jpg");
+
+        reference.add(myRestaurent).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Restaurent Uploaded", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry nit success ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
