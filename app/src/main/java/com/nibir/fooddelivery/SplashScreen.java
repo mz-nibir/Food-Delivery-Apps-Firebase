b306f0841695124c713c5d7a72adf2925804d424
Nibir <mz.nibir@gmail.com>
2020-11-01 15:34:13 +0600

    finish added

--- app/src/main/java/com/nibir/fooddelivery/SplashScreen.java
+++ app/src/main/java/com/nibir/fooddelivery/SplashScreen.java
@@ -20,6 +20,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
 
             }
         }, delayMillis= 3000);
