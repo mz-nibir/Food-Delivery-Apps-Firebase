cdf74c2714030f27d5c546dd8184bbe4a6955bc4
Nibir <mz.nibir@gmail.com>
2020-11-01 23:30:13 +0600

    restaurent class update

--- app/src/main/java/com/nibir/fooddelivery/model/Restaurant.java
+++ app/src/main/java/com/nibir/fooddelivery/model/Restaurant.java
@@ -1,4 +1,55 @@
 package com.nibir.fooddelivery.model;
 
+import java.util.List;
+
 public class Restaurant {
+    String restaurentName;
+    String restaurentDescription;
+    String restaurentImgUrl;
+    String restaurentLocation;
+
+    List<MenuItem> restaurentMenuList;
+
+    public Restaurant() {
+    }
+
+    public String getRestaurentName() {
+        return restaurentName;
+    }
+
+    public void setRestaurentName(String restaurentName) {
+        this.restaurentName = restaurentName;
+    }
+
+    public String getRestaurentDescription() {
+        return restaurentDescription;
+    }
+
+    public void setRestaurentDescription(String restaurentDescription) {
+        this.restaurentDescription = restaurentDescription;
+    }
+
+    public String getRestaurentImgUrl() {
+        return restaurentImgUrl;
+    }
+
+    public void setRestaurentImgUrl(String restaurentImgUrl) {
+        this.restaurentImgUrl = restaurentImgUrl;
+    }
+
+    public String getRestaurentLocation() {
+        return restaurentLocation;
+    }
+
+    public void setRestaurentLocation(String restaurentLocation) {
+        this.restaurentLocation = restaurentLocation;
+    }
+
+    public List<MenuItem> getRestaurentMenuList() {
+        return restaurentMenuList;
+    }
+
+    public void setRestaurentMenuList(List<MenuItem> restaurentMenuList) {
+        this.restaurentMenuList = restaurentMenuList;
+    }
 }
