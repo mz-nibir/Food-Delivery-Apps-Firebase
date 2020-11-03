918b1d9ed99c9bf276b2ce18ba0007acc32e3c50
Nibir <mz.nibir@gmail.com>
2020-11-03 21:29:32 +0600

    user login update

--- app/src/main/java/com/nibir/fooddelivery/SplashScreen.java
+++ app/src/main/java/com/nibir/fooddelivery/SplashScreen.java
@@ -7,19 +7,32 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
+    FirebaseUser user;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        auth=FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
         Handler handeler = new Handler();
         long delayMillis;
         handeler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                if(user!=null){
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }else {
+                    startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+
+                }
                 finish();
 
             }
