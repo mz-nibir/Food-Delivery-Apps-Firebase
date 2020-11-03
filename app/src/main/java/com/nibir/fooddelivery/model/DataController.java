f70360cf47c54a240b7e1637e181da6b666dc7fd
Nibir <mz.nibir@gmail.com>
2020-11-03 11:30:35 +0600

    data controller update

--- app/src/main/java/com/nibir/fooddelivery/model/DataController.java
+++ app/src/main/java/com/nibir/fooddelivery/model/DataController.java
@@ -1,4 +1,25 @@
 package com.nibir.fooddelivery.model;
 
 public class DataController {
+    public  static  DataController instance;
+
+    public static DataController getInstance(){
+        if (instance==null)
+            instance= new DataController();
+
+        return instance;
+
+    }
+
+
+    RestaurantInterface restaurantInterface;
+
+    public RestaurantInterface getRestaurantInterface() {
+        return restaurantInterface;
+    }
+
+    public void setRestaurantInterface(RestaurantInterface restaurantInterface) {
+        this.restaurantInterface = restaurantInterface;
+    }
 }
+
