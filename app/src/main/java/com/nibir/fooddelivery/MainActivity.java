567765c11f1e7e77cabf75003c98c1c1c1149feb
Nibir <mz.nibir@gmail.com>
2020-11-04 23:12:07 +0600

    Restaurant added

--- app/src/main/java/com/nibir/fooddelivery/MainActivity.java
+++ app/src/main/java/com/nibir/fooddelivery/MainActivity.java
@@ -55,7 +55,11 @@ public class MainActivity extends AppCompatActivity implements RestaurantInterfa
         NavigationUI.setupWithNavController(navView, navController);
 
 
-      SentDataToFireStore();
+//      SentDataToFireStore();
+//      SentDataToFireStoreA();
+//      SentDataToFireStoreB();
+//      SentDataToFireStoreC();
+//      SentDataToFireStoreD();
 
 
     }
@@ -69,13 +73,13 @@ public class MainActivity extends AppCompatActivity implements RestaurantInterfa
 
         CollectionReference reference = db.collection("Restaurants");
         Restaurant myRestaurant = new Restaurant();
-        myRestaurant.setRestaurantName("Nibir Restaurants");
-        myRestaurant.setRestaurantDescription("Best restaurant in Dhaka");
-        myRestaurant.setRestaurantLocation("Bashundhara,Dhaka");
-        myRestaurant.setRestaurantImgUrl("https://img.jakpost.net/c/2016/09/29/2016_09_29_12990_1475116504._large.jpg");
+        myRestaurant.setRestaurantName("Sultan Dines");
+        myRestaurant.setRestaurantDescription("Mirpur-10, Dhaka");
+        myRestaurant.setRestaurantLocation("Bangladesh");
+        myRestaurant.setRestaurantImgUrl("https://binfo.com.bd/uploads/images/54729351_2226449311004948_4440402280281276416_n.jpg");
 
         List<MenuItem> myMenus = new ArrayList<>();
-        for (int i=0;i <15;i++){
+        for (int i=0;i <10;i++){
             myMenus.add(new MenuItem("Mutton Kacchi","Khub e Testy",350));
 
         }
@@ -95,6 +99,145 @@ public class MainActivity extends AppCompatActivity implements RestaurantInterfa
 
 
     }
+
+    //added
+
+    private void SentDataToFireStoreA() {
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+
+        CollectionReference reference = db.collection("Restaurants");
+        Restaurant myRestaurant = new Restaurant();
+        myRestaurant.setRestaurantName("Hungry Eyes");
+        myRestaurant.setRestaurantDescription("Mirpur-11, Dhaka");
+        myRestaurant.setRestaurantLocation("Bangladesh");
+        myRestaurant.setRestaurantImgUrl("https://img.jakpost.net/c/2016/09/29/2016_09_29_12990_1475116504._large.jpg");
+
+        List<MenuItem> myMenus = new ArrayList<>();
+        for (int i=0;i <10;i++){
+            myMenus.add(new MenuItem("Mutton Kacchi","Khub e Testy",350));
+
+        }
+        myRestaurant.setRestaurantMenuList(myMenus);
+
+
+        reference.add(myRestaurant).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Restaurant Uploaded", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry not success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void SentDataToFireStoreB() {
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+
+        CollectionReference reference = db.collection("Restaurants");
+        Restaurant myRestaurant = new Restaurant();
+        myRestaurant.setRestaurantName("Rabbani Hotel & Restaurant");
+        myRestaurant.setRestaurantDescription("Best restaurant in Dhaka");
+        myRestaurant.setRestaurantLocation("Mirpur-10, Dhaka");
+        myRestaurant.setRestaurantImgUrl("https://media-cdn.tripadvisor.com/media/photo-s/17/b6/d1/d4/dhakaregencyhotel.jpg");
+
+        List<MenuItem> myMenus = new ArrayList<>();
+        for (int i=0;i <10;i++){
+            myMenus.add(new MenuItem("Mutton Kacchi","Khub e Testy",350));
+
+        }
+        myRestaurant.setRestaurantMenuList(myMenus);
+
+
+        reference.add(myRestaurant).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Restaurant Uploaded", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry not success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void SentDataToFireStoreC() {
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+
+        CollectionReference reference = db.collection("Restaurants");
+        Restaurant myRestaurant = new Restaurant();
+        myRestaurant.setRestaurantName("Hazi Biriani");
+        myRestaurant.setRestaurantDescription("Old Dhaka");
+        myRestaurant.setRestaurantLocation("Bangladesh");
+        myRestaurant.setRestaurantImgUrl("https://2.bp.blogspot.com/-IjmGdjYRsyc/WUp3gXjWxjI/AAAAAAAAAAg/dceUL-NX1kAGNy37DcazKPadVqewJtcNwCPcBGAYYCw/s1600/y.jpg");
+
+        List<MenuItem> myMenus = new ArrayList<>();
+        for (int i=0;i <10;i++){
+            myMenus.add(new MenuItem("Mutton Kacchi","Khub e Testy",350));
+
+        }
+        myRestaurant.setRestaurantMenuList(myMenus);
+
+
+        reference.add(myRestaurant).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Restaurant Uploaded", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry not success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void SentDataToFireStoreD() {
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+
+        CollectionReference reference = db.collection("Restaurants");
+        Restaurant myRestaurant = new Restaurant();
+        myRestaurant.setRestaurantName("Astha Cafe");
+        myRestaurant.setRestaurantDescription("Sector-12, Uttora");
+        myRestaurant.setRestaurantLocation("Bangladesh");
+        myRestaurant.setRestaurantImgUrl("https://i2-prod.lincolnshirelive.co.uk/incoming/article3579859.ece/ALTERNATES/s615/3_DHR_HMB_cookhousexams_261119.jpg");
+
+        List<MenuItem> myMenus = new ArrayList<>();
+        for (int i=0;i <10;i++){
+            myMenus.add(new MenuItem("Mutton Kacchi","Khub e Testy",350));
+
+        }
+        myRestaurant.setRestaurantMenuList(myMenus);
+
+
+        reference.add(myRestaurant).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Restaurant Uploaded", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry not success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+
 
     @Override
     public void onRestaurentClick(Restaurant restaurant) {
